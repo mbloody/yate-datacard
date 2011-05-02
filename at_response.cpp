9d5294db515bb01c58c96c686ac79774a839be30
mc.creat <mc.creat@9fbce5c6-81f1-db7a-3ab1-475d10aa6bdd>
2011-05-02 15:53:34 +0000

    Attached notices to the program. Some changes in readme file
    git-svn-id: http://yate-datacard.googlecode.com/svn/trunk@128 9fbce5c6-81f1-db7a-3ab1-475d10aa6bdd

--- at_response.cpp
+++ at_response.cpp
@@ -1,11 +1,29 @@
-/* 
-   Copyright (C) 2009 - 2010
-   
-   Artem Makhutov <artem@makhutov.org>
-   http://www.makhutov.org
-   
-   Dmitry Vagin <dmitry2004@yandex.ru>
-*/
+/**
+ * at_response.cpp
+ * This file is part of the Yate-datacard Project http://code.google.com/p/yate-datacard/
+ * Yate datacard channel driver for Huawei UMTS modem
+ *
+ * Copyright (C) 2010-2011 MBloody
+ *
+ * Based on chan_datacard module for Asterisk
+ *
+ * Copyright (C) 2009-2010 Artem Makhutov <artem@makhutov.org>
+ * Copyright (C) 2009-2010 Dmitry Vagin <dmitry2004@yandex.ru>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
 
 #include "datacarddevice.h"
 #include <stdio.h>
