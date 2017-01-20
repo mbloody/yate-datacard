48ad193ceec4e02ca4ce9d8d1a2a258c99d1391c
Dmitry Bessonov <megadoz@gmail.com>
2017-01-20 13:53:58 +0700

    install config to correct dest

--- cmake/FindYate.cmake
+++ cmake/FindYate.cmake
@@ -1,25 +1,47 @@
+# rst:
+# FindYate
+# --------
+#
+# Find YATE include file and libraries
+#
+# Result variables
+#
+# YATE_FOUND - system has yate installed
+# YATE_INCLUDE_DIR - the YATE include directory
+# YATE_LIBRARIES - 
+# YATE_VERSION_STRING -
+# YATE_MODULES_DIR - 
+# YATE_CONFIG_DIR - 
+# 
+#
+################################################################################
 
-find_package(PkgConfig)
 
-pkg_check_modules(PC_YATE REQUIRED yate)
-set(YATE_DEFINITIONS ${PC_YATE_CFLAGS_OTHER})
+# find yate-config
+#
+find_program(YATE_CONFIG_EXECUTABLE NAMES yate-config DOC "yate-config executable")
+
+if(YATE_CONFIG_EXECUTABLE)
+	execute_process(COMMAND ${YATE_CONFIG_EXECUTABLE} --version
+			OUTPUT_VARIABLE YATE_VERSION_STRING
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+	execute_process(COMMAND ${YATE_CONFIG_EXECUTABLE} --modules
+			OUTPUT_VARIABLE YATE_MODULES_DIR
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+	execute_process(COMMAND ${YATE_CONFIG_EXECUTABLE} --config
+			OUTPUT_VARIABLE YATE_CONFIG_DIR
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
 
 find_path(YATE_INCLUDE_DIR yatengine.h
-          HINTS ${PC_YATE_INCLUDE_DIR} ${PC_YATE_INCLUDE_DIRS}
-          PATH_SUFFIXES yate )
+	PATH_SUFFIXES yate)
 
-find_library(YATE_LIBRARY NAMES yate
-             HINTS ${PC_YATE_LIBDIR} ${PC_YATE_LIBRARY_DIRS} )
+message(${YATE_INCLUDE_DIR})
 
-set(YATE_LIBRARIES ${YATE_LIBRARY} )
-set(YATE_INCLUDE_DIRS ${YATE_INCLUDE_DIR} )
+find_library(YATE_LIBRARY NAMES yate)
 
-set(YATE_MODULES_DIR ${PC_YATE_LIBDIR}/yate)
+set(YATE_LIBRARIES ${YATE_LIBRARY})
 
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set YATE_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(Yate  DEFAULT_MSG
-                                  YATE_LIBRARY YATE_INCLUDE_DIR)
-
-mark_as_advanced(YATE_INCLUDE_DIR YATE_LIBRARY )
+mark_as_advanced(YATE_CONFIG_DIR YATE_MODULES_DIR YATE_LIBRARY_DIR YATE_INCLUDE_DIR)
